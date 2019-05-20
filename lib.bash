81aa92fee91b5cd076c64c5dae477669cd16566a
Michael Schubert <schu@schu.io>
2019-05-20 17:49:58 +0200

    lib: remove redundant `ca-config.json` creation

--- lib.bash
+++ lib.bash
@@ -459,21 +459,6 @@ kubedee::create_certificate_authority_etcd() {
   ]
 }
 EOF
-    cat >ca-config.json <<EOF
-{
-  "signing": {
-    "default": {
-      "expiry": "8760h"
-    },
-    "profiles": {
-      "kubernetes": {
-        "usages": ["signing", "key encipherment", "server auth", "client auth"],
-        "expiry": "8760h"
-      }
-    }
-  }
-}
-EOF
   )
 }
 
