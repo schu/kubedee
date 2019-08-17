d7fae30bccd60c5fe6ee798038ea84a9649c8e00
Michael Schubert <schu@schu.io>
2019-08-17 12:34:18 +0200

    lib: update etcd v3.3.13 -> v3.3.14

--- lib.bash
+++ lib.bash
@@ -56,7 +56,7 @@ kubedee::exit_error() {
 
 readonly kubedee_base_image="ubuntu:18.04"
 readonly kubedee_container_image="kubedee-container-image-${kubedee_version//[._]/-}"
-readonly kubedee_etcd_version="v3.3.13"
+readonly kubedee_etcd_version="v3.3.14"
 readonly kubedee_runc_version="v1.0.0-rc8"
 readonly kubedee_cni_plugins_version="v0.8.0"
 readonly kubedee_crio_version="v1.14.0"
