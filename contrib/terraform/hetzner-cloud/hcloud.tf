91695c4c90e9e1bff993e534c9a49d7e00c75058
Michael Schubert <schu@schu.io>
2021-05-22 17:02:46 +0200

    contrib/terraform/hetzner-cloud: updates for terraform 0.15
    A few things changed in terraform and require an update. For example,
    default connection blocks have been removed and provider configuration
    is now required. Update the code to make it work with terraform 0.15.

--- contrib/terraform/hetzner-cloud/hcloud.tf
+++ contrib/terraform/hetzner-cloud/hcloud.tf
@@ -37,11 +37,8 @@ resource "hcloud_server" "test" {
 
     connection {
       user = "root"
+      host = self.ipv4_address
     }
-  }
-
-  connection {
-    user = "ubuntu"
   }
 
   provisioner "remote-exec" {
@@ -49,19 +46,39 @@ resource "hcloud_server" "test" {
       "mkdir -p /home/ubuntu/bin",
       "mkdir -p /home/ubuntu/kubedee",
     ]
+
+    connection {
+      user = "ubuntu"
+      host = self.ipv4_address
+    }
   }
 
   provisioner "file" {
     source      = "../../../kubedee"
     destination = "/home/ubuntu/kubedee/kubedee"
+
+    connection {
+      user = "ubuntu"
+      host = self.ipv4_address
+    }
   }
   provisioner "file" {
     source      = "../../../lib.bash"
     destination = "/home/ubuntu/kubedee/lib.bash"
+
+    connection {
+      user = "ubuntu"
+      host = self.ipv4_address
+    }
   }
   provisioner "file" {
     source      = "../../../manifests"
     destination = "/home/ubuntu/kubedee/"
+
+    connection {
+      user = "ubuntu"
+      host = self.ipv4_address
+    }
   }
 
   provisioner "remote-exec" {
@@ -77,21 +94,31 @@ resource "hcloud_server" "test" {
       "ln -s /home/ubuntu/kubedee/kubedee bin/kubedee",
 
       # Create cluster
-      "./bin/kubedee up --apiserver-extra-hostnames '${hcloud_server.test.name},${hcloud_server.test.ipv4_address}' --kubernetes-version '${var.kubernetes_version}' test",
+      "./bin/kubedee up --apiserver-extra-hostnames '${hcloud_server.test[count.index].name},${hcloud_server.test[count.index].ipv4_address}' --kubernetes-version '${var.kubernetes_version}' test",
 
       # Setup ingress
       "kubectl apply -f kubedee/manifests/ingress-nginx.yml",
 
       # Make ingress-nginx accessible
-      "sudo iptables -t nat -A PREROUTING -p tcp -d ${hcloud_server.test.ipv4_address} --dport 80 -j DNAT --to-destination $(./bin/kubedee controller-ip test):80",
-      "sudo iptables -t nat -A PREROUTING -p tcp -d ${hcloud_server.test.ipv4_address} --dport 443 -j DNAT --to-destination $(./bin/kubedee controller-ip test):443",
+      "sudo iptables -t nat -A PREROUTING -p tcp -d ${hcloud_server.test[count.index].ipv4_address} --dport 80 -j DNAT --to-destination $(./bin/kubedee controller-ip test):80",
+      "sudo iptables -t nat -A PREROUTING -p tcp -d ${hcloud_server.test[count.index].ipv4_address} --dport 443 -j DNAT --to-destination $(./bin/kubedee controller-ip test):443",
 
       # Make apiserver accessible
-      "sudo iptables -t nat -A PREROUTING -p tcp -d ${hcloud_server.test.ipv4_address} --dport 6443 -j DNAT --to-destination $(./bin/kubedee controller-ip test):6443",
+      "sudo iptables -t nat -A PREROUTING -p tcp -d ${hcloud_server.test[count.index].ipv4_address} --dport 6443 -j DNAT --to-destination $(./bin/kubedee controller-ip test):6443",
     ]
+
+    connection {
+      user = "ubuntu"
+      host = self.ipv4_address
+    }
   }
 
   provisioner "local-exec" {
-    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${hcloud_server.test.ipv4_address}:~/.local/share/kubedee/clusters/test/kubeconfig/admin.kubeconfig . && sed -i -e 's|server:.*|server: https://${hcloud_server.test.ipv4_address}:6443|' admin.kubeconfig"
+    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${hcloud_server.test[count.index].ipv4_address}:~/.local/share/kubedee/clusters/test/kubeconfig/admin.kubeconfig . && sed -i -e 's|server:.*|server: https://${hcloud_server.test[count.index].ipv4_address}:6443|' admin.kubeconfig"
+
+    connection {
+      user = "ubuntu"
+      host = self.ipv4_address
+    }
   }
 }
