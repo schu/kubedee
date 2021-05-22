9dadca5a854512be367f7e5eb69e325005d5c198
Michael Schubert <schu@schu.io>
2021-05-22 17:27:00 +0200

    contrib/terraform/hetzner-cloud: use latest cfssl version

--- contrib/terraform/hetzner-cloud/user-data.bash
+++ contrib/terraform/hetzner-cloud/user-data.bash
@@ -19,9 +19,9 @@ lxd init --auto --storage-backend btrfs
 
 usermod -a -G lxd,sudo ubuntu
 
-curl -fsSL https://github.com/cloudflare/cfssl/releases/download/v1.4.1/cfssl_1.4.1_linux_amd64 -o /tmp/cfssl &&
+curl -fsSL https://github.com/cloudflare/cfssl/releases/download/v1.5.0/cfssl_1.5.0_linux_amd64 -o /tmp/cfssl &&
   install -m 0755 /tmp/cfssl /usr/local/bin/
-curl -fsSL https://github.com/cloudflare/cfssl/releases/download/v1.4.1/cfssl_1.4.1_linux_amd64 -o /tmp/cfssljson &&
+curl -fsSL https://github.com/cloudflare/cfssl/releases/download/v1.5.0/cfssljson_1.5.0_linux_amd64 -o /tmp/cfssljson &&
   install -m 0755 /tmp/cfssljson /usr/local/bin/
 
 readonly k8s_latest="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
